cee9f6ac905b2dde32a14641b6646dff7c84e26f
Laryn Qi <larynqi@berkeley.edu>
2020-09-22 00:43:40 -0700

    test audio

--- js/announcements.js
+++ js/announcements.js
@@ -1,7 +1,7 @@
 var today = new Date(),
     day = today.getDate(),
     month = today.getMonth() + 1;
-if (month == 4 && day == 1) {
+if (month == 9 && day == 22) {
     alert('A N N O U N C E M E N T S');
     document.getElementById("a n n o u n c e m e n t s").style.fontSize = "9rem";
     var denero = document.getElementById("denero")
@@ -9,4 +9,5 @@ if (month == 4 && day == 1) {
     denero.style.maxHeight = "800px";
     denero.style.left = "45vw";
     denero.style.top = "5vw";
+    document.getElementById("denero-audio").play();
 }
