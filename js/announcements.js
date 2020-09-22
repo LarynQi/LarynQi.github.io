81527af61af4f68a5ab3dd5fb2c3eb74b29da4eb
Laryn Qi <larynqi@berkeley.edu>
2020-09-22 00:02:51 -0700

    update links to course websites

--- js/announcements.js
+++ js/announcements.js
@@ -3,5 +3,10 @@ var today = new Date(),
     month = today.getMonth() + 1;
 if (month == 4 && day == 1) {
     alert('A N N O U N C E M E N T S');
-	document.getElementById("a n n o u n c e m e n t s").style.fontSize = "9rem";
+    document.getElementById("a n n o u n c e m e n t s").style.fontSize = "9rem";
+    var denero = document.getElementById("denero")
+    denero.style.maxWidth = "600px";
+    denero.style.maxHeight = "800px";
+    denero.style.left = "45vw";
+    denero.style.top = "5vw";
 }
