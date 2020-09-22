d5d7ea27bed30abb8dc816f8f9d4094586f68c27
Laryn Qi <larynqi@berkeley.edu>
2020-09-21 22:57:28 -0700

    first update 9/21/20

---
+++ js/announcements.js
@@ -0,0 +1,7 @@
+var today = new Date(),
+    day = today.getDate(),
+    month = today.getMonth() + 1;
+if (month == 4 && day == 1) {
+    alert('A N N O U N C E M E N T S');
+	document.getElementById("a n n o u n c e m e n t s").style.fontSize = "9rem";
+}
