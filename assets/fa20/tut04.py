a513f67aeffa3cb4d1d707fc280871d68b0cb544
Laryn Qi <larynqi@berkeley.edu>
2020-09-23 14:00:49 -0700

    upload tut04.py

---
+++ assets/fa20/tut04.py
@@ -0,0 +1,18 @@
+"""
+run:
+
+python3 -m doctest -v tut04.py
+
+"""
+
+def count_k(n, k):
+    """
+    >>> count_k(3, 3) # 3, 2 + 1, 1 + 2, 1 + 1 + 1
+    4
+    >>> count_k(4, 4)
+    8
+    >>> count_k(10, 3)
+    274
+    >>> count_k(300, 1) # Only one step at a time
+    1
+    """
