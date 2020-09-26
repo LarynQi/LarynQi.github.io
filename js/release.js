7ae351c43d97e9ed1fbb70c6d57619b9f884193e
Laryn Qi <larynqi@berkeley.edu>
2020-09-26 03:52:01 -0700

    auto release csm61a week06 wkst

--- js/release.js
+++ js/release.js
@@ -1,8 +1,15 @@
 var today = new Date(),
     day = today.getDate(),
-    month = today.getMonth() + 1;
-if (month == 9 && day >= 26) {
-    var sol06 = document.getElementById("61a-06");
+    month = today.getMonth() + 1,
+    hour = today.getHours();
+if (month == 10 && day >= 1) {
+    var sol06 = document.getElementById("61a-06-sol");
+    if (day == 1) {
+        if (hour >= 12) {
+            sol06.setAttribute("href", "assets/fa20-csm/61a-6-sol.pdf");
+        }
+    } else {
+        sol06.setAttribute("href", "assets/fa20-csm/61a-6-sol.pdf");
+    }
     // sol06.href = "assets/fa20-csm/61a-6-sol.pdf";
-    sol06.setAttribute("href", "assets/fa20-csm/61a-6-sol.pdf");
 }
