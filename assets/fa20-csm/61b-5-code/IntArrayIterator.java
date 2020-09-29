0e75631a640e963ece99c9f10a83464b76d6ef5d
Laryn Qi <larynqi@berkeley.edu>
2020-09-29 12:23:00 -0700

    update IntArrayIterator.java

--- assets/fa20-csm/61b-5-code/IntArrayIterator.java
+++ assets/fa20-csm/61b-5-code/IntArrayIterator.java
@@ -1,32 +1,34 @@
 public class IntArrayIterator implements IntIterator {
-	private int index;
-	private int[] array;
+    private int index;
+    private int[] array;
 
-	public IntArrayIterator(int[] arr) {
-		array = arr;
-	}
+    public IntArrayIterator(int[] arr) {
+        array = arr;
+        // following line is optional
+        index = 0;
+    }
 
-	public boolean hasNext() {
-		return index < array.length;
-	}
+    public boolean hasNext() {
+        return index < array.length;
+    }
 
-	public int next() {
-		return array[index];
-		// int value = array[index];
-		// index += 1;
-		// return value;
-	}
-	public static void main(String[] args) {
+    public int next() {
+        int value = array[index];
+        index += 1;
+        return value;
+        // return array[index++];
+    }
+    public static void main(String[] args) {
         int[] arr ={1, 2, 3, 4, 5, 6};
         IntIterator iter = new IntArrayIterator(arr);
         if (iter.hasNext()) {
-        	System.out.println(iter.next());
+            System.out.println(iter.next());
         }
         if (iter.hasNext()) {
-        	System.out.println(iter.next() + 3);
+            System.out.println(iter.next() + 3);
         }
         while (iter.hasNext()) {
-        	System.out.println(iter.next());
+            System.out.println(iter.next());
         }
     }
 }
