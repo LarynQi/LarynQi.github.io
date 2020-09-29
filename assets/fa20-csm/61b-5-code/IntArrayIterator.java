496f6404a2cf62e50ccb14cea11d370bdb8ea60c
Laryn Qi <larynqi@berkeley.edu>
2020-09-29 12:11:27 -0700

    actually upload code

---
+++ assets/fa20-csm/61b-5-code/IntArrayIterator.java
@@ -0,0 +1,32 @@
+public class IntArrayIterator implements IntIterator {
+	private int index;
+	private int[] array;
+
+	public IntArrayIterator(int[] arr) {
+		array = arr;
+	}
+
+	public boolean hasNext() {
+		return index < array.length;
+	}
+
+	public int next() {
+		return array[index];
+		// int value = array[index];
+		// index += 1;
+		// return value;
+	}
+	public static void main(String[] args) {
+        int[] arr ={1, 2, 3, 4, 5, 6};
+        IntIterator iter = new IntArrayIterator(arr);
+        if (iter.hasNext()) {
+        	System.out.println(iter.next());
+        }
+        if (iter.hasNext()) {
+        	System.out.println(iter.next() + 3);
+        }
+        while (iter.hasNext()) {
+        	System.out.println(iter.next());
+        }
+    }
+}
